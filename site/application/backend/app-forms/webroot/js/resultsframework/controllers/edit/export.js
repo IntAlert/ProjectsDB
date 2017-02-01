b1aa5b206ae3377790e95e1ad1b0fc743138cd66
Alan Thomson <alan@projectmccoll.co.uk>
2017-02-01 17:03:08 +0100

    download from export tab

--- site/application/backend/app-forms/webroot/js/resultsframework/controllers/edit/export.js
+++ site/application/backend/app-forms/webroot/js/resultsframework/controllers/edit/export.js
@@ -34,49 +34,49 @@ app.controller('ExportController', function($scope, $window, $mdDialog, Training
 
 	$scope.exportTrainingData = function() {
 		
-		var url = TrainingsService.api_urls.csv;
+		var url = TrainingsService.api_urls.csv + '&download=1';
 		$window.open(url, '_blank');
 	}
 
 	$scope.exportAccompanimentData = function() {
 		
-		var url = AccompanimentsService.api_urls.csv;
+		var url = AccompanimentsService.api_urls.csv + '&download=1';
 		$window.open(url, '_blank');
 	}
 
 	$scope.exportDialogueProcessData = function() {
 		
-		var url = ProcessesService.api_urls.csv;
+		var url = ProcessesService.api_urls.csv + '&download=1';
 		$window.open(url, '_blank');
 	}
 
 	$scope.exportDialogueMeetingData = function() {
 		
-		var url = MeetingsService.api_urls.csv;
+		var url = MeetingsService.api_urls.csv + '&download=1';
 		$window.open(url, '_blank');
 	}
 
 	$scope.exportResearchData = function() {
 		
-		var url = ResearchesService.api_urls.csv;
+		var url = ResearchesService.api_urls.csv + '&download=1';
 		$window.open(url, '_blank');
 	}
 
 	$scope.exportAdvocacyData = function() {
 		
-		var url = AdvocaciesService.api_urls.csv;
+		var url = AdvocaciesService.api_urls.csv + '&download=1';
 		$window.open(url, '_blank');
 	}
 
 	$scope.exportOtherActivityData = function() {
 		
-		var url = OtherActivitiesService.api_urls.csv;
+		var url = OtherActivitiesService.api_urls.csv + '&download=1';
 		$window.open(url, '_blank');
 	}
 
 	$scope.exportResultData = function() {
 		
-		var url = ResultsService.api_urls.csv;
+		var url = ResultsService.api_urls.csv + '&download=1';
 		$window.open(url, '_blank');
 	}
 
